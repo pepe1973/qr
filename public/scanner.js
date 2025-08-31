53c98420428469d71462315f48d725d1b4f981aa
pepe1973 <bodrogipr@gmail.com>
2025-08-31 18:17:18 +0200

    2025_08_31 Mentés

--- public/scanner.js
+++ public/scanner.js
@@ -1,36 +1,73 @@
-let response = Html5Qrcode.getCameras()
-    .then((devices) => {
-        let cameraId = 0;
-        if (devices && devices.length) {
-            cameraId = devices[0].id;
-            console.log(cameraId);
-        }
+// let response = Html5Qrcode.getCameras()
+//     .then((devices) => {
+//         let cameraId = 0;
+//         if (devices && devices.length) {
+//             cameraId = devices[0].id;
+//             console.log(cameraId);
+//         }
 
-        return cameraId;
-    })
-    .then((id) => {
-        console.log(id);
-    })
-    .catch((err) => {
-        console.log(err);
+//         return cameraId;
+//     })
+//     .then((id) => {
+//         console.log(id);
+//     })
+//     .catch((err) => {
+//         console.log(err);
+//     });
+
+// const html5QrCode = new Html5Qrcode('reader');
+
+// html5QrCode
+//     .start(
+//         'a759ea82e36966c03fd82e370ad1016f2b73f8af5a51d5ca8ba4e04bf28b7917',
+//         {
+//             fps: 30, // Optional, frame per seconds for qr code scanning
+//             qrbox: { width: 250, height: 250 }, // Optional, if you want bounded box UI
+//         },
+//         (decodedText, decodedResult) => {
+//             console.log(`Code matched = ${decodedText}`, decodedResult);
+//         },
+//         (errorMessage) => {
+//             // console.log(errorMessage);
+//         }
+//     )
+//     .catch((err) => {
+//         console.log(err);
+//     });
+
+// script.js file
+
+function domReady(fn) {
+    if (
+        document.readyState === 'complete' ||
+        document.readyState === 'interactive'
+    ) {
+        setTimeout(fn, 1000);
+    } else {
+        document.addEventListener('DOMContentLoaded', fn);
+    }
+}
+
+domReady(function () {
+    // If found you qr code
+    async function onScanSuccess(decodeText, decodeResult) {
+        alert('Tanuló: ' + decodeText);
+        console.log('Tanuló: ' + decodeText);
+        const response = await fetch('/filebakiir', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ nev: decodeText }),
+        });
+
+        const data = await response.json();
+        console.log(data);
+    }
+
+    let htmlscanner = new Html5QrcodeScanner('my-qr-reader', {
+        fps: 10,
+        qrbos: 250,
     });
-
-const html5QrCode = new Html5Qrcode('reader');
-
-html5QrCode
-    .start(
-        'a759ea82e36966c03fd82e370ad1016f2b73f8af5a51d5ca8ba4e04bf28b7917',
-        {
-            fps: 30, // Optional, frame per seconds for qr code scanning
-            qrbox: { width: 250, height: 250 }, // Optional, if you want bounded box UI
-        },
-        (decodedText, decodedResult) => {
-            console.log(`Code matched = ${decodedText}`, decodedResult);
-        },
-        (errorMessage) => {
-            // console.log(errorMessage);
-        }
-    )
-    .catch((err) => {
-        console.log(err);
-    });
+    htmlscanner.render(onScanSuccess);
+});
--- server.js
+++ server.js
@@ -6,10 +6,38 @@ const fsPromise = require('node:fs/promises');
 const app = express();
 const PORT = 5000;
 app.use(express.static(path.resolve(__dirname, 'public')));
+app.use(express.json());
 
 app.get('/', (req, res) => {
     try {
         return res.status(200).render('index.ejs');
+    } catch (error) {
+        return res.status(500).json({ msg: 'Hiba: ' + error.message });
+    }
+});
+
+app.post('/filebakiir', async (req, res) => {
+    const { nev } = req.body;
+    const nap = new Date().getDate();
+    const honap = new Date().getMonth();
+    const ev = new Date().getFullYear();
+    const ora = new Date().getHours();
+    const perc = new Date().getMinutes();
+    const masodperc = new Date().getSeconds();
+    const idopont = `${ev}_${honap + 1}_${nap}.csv`;
+
+    try {
+        const filenev = path.resolve(__dirname, 'public', 'napok', idopont);
+        const tartalom = `${nev.split(';')[0]};${
+            nev.split(';')[1]
+        };${ora}:${perc}:${masodperc}`;
+
+        const noveltTartalom = tartalom + '\n';
+
+        await fsPromise.appendFile(filenev, '\ufeff' + noveltTartalom, {
+            encoding: 'utf-8',
+        });
+        return res.status(201).json({ msg: 'Sikeres file-ba írás!' });
     } catch (error) {
         return res.status(500).json({ msg: 'Hiba: ' + error.message });
     }
